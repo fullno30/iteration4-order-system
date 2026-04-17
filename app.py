861f04968a3da6e5ca9e90ff45ed8f1991d49bed
fullno30 <161750069+fullno30@users.noreply.github.com>
2026-04-17 10:18:45 -0500

    Add startup functions for test data initialization
    Added startup functions to initialize test data for products and orders.

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
+# start up test data
 def startup_data():
   p_repo.add_product(Product(1, "Mug", 12.50))
   p_repo.add_product(Product(2, "Scarf", 25.00))
 
+startup_data()
+
+# start up order test data
 def startup_order():
   items = [
     {"id": 1, "qty": 2},
     {"id": 2, "qty": 1}
   ]
   service.create_order(items)
+
+startup_order()
