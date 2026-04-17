d76e5ce0f1de4b1f7c87a897d6439070ffc1a94f
fullno30 <161750069+fullno30@users.noreply.github.com>
2026-04-17 10:36:45 -0500

    Implement OrderRepository with basic CRUD methods

---
+++ repository/order_repo.py
@@ -0,0 +1,12 @@
+class OrderRepository:
+  def __init__(self):
+    self.orders = {}
+
+  def save(self, order):
+    self.orders[order.id] = order
+
+  def get(self, order_id):
+    return self.orders,get(order_id)
+
+  def list_all(self):
+    return list(self.orders.values())
